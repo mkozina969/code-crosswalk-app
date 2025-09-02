8dbab762c11e5b023a04bf4fd87a7fdfa139a5d0
mkozina969 <mkozina@intercars.eu>
2025-09-02 22:54:25 +0200

    Update rebuild_db.bat

--- rebuild_db.bat
+++ rebuild_db.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Rebuilding crosswalk.db from crosswalk.csv ...
-python make_crosswalk_db.py --csv crosswalk.csv --db data\crosswalk.db --rebuild
+py make_crosswalk_db.py --csv crosswalk.csv --db data\crosswalk.db --rebuild
 echo Done! Database is ready at data\crosswalk.db
 pause
